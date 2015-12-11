a4339e1922f45ea7137518cfc40b32edf1c3d4f8
Pascal P <pascal.phelipot@gmail.com>
2015-12-11 09:17:01 +0100

    Documentation et simplification

--- src/Modele.java
+++ src/Modele.java
@@ -36,16 +36,17 @@ public class Modele{
     public void loadScores(){
         String base = "./scores_";
         for (int i =0; i<scores.length ; i++){
+            //On ouvre le fichier de score correspondant à chaque taille (4, 5, 6)
             File scoreF =new File(base+(i+4)+"x"+(i+4)+".dat");
             try{
-                //on lit les scores
+                //on lit les scores (3 entiers)
                 DataInputStream fichier = new DataInputStream(new BufferedInputStream(new FileInputStream(scoreF)));
                 for (int j=0; j<3; j++){
                     scores[i][j] = fichier.readInt();
                 }
             }catch(IOException e){
                 System.out.println("Attention : les scores n'ont pas pu être chargés !");
-                //on met à zéro les trois scores
+                //on met à zéro les trois scores si on a pas pu les charger
                 for (int j=0; j<3; j++)
                     scores[i][j]=0;
             }
@@ -58,9 +59,10 @@ public class Modele{
     public void writeScores(){
         String base = "./scores_";
         for (int i =0; i<scores.length ; i++){
+            //On ouvre le fichier de score correspondant à chaque taille (4, 5, 6)
             File scoreF =new File(base+(i+4)+"x"+(i+4)+".dat");
             try{
-                //on lit les scores
+                //on ecrit les 3 scores
                 DataOutputStream fichier = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(scoreF)));
                 for (int s: scores[i]){
                     fichier.writeInt(s);
@@ -74,7 +76,7 @@ public class Modele{
     }
 
     /**
-     * Tente d'insérer le score actuel dans les meilleurs scores. Retourne True après avoir sauvegarder les scores si c'est le cas.
+     * Tente d'insérer le score actuel dans les meilleurs scores. Retourne True après avoir sauvegardé les scores si c'est le cas.
      * @return
      */
     public boolean submitScore(){
@@ -87,12 +89,18 @@ public class Modele{
             }
         }
         if (tmp != scoreActuel()){
+            //On a changé le classement, il faut le sauvegarder dans les fichiers de score !
             writeScores();
             return true;
         }
         return false;
     }
 
+    /**
+     * Renvoie les meilleurs scores de la taille demandée
+     * @param taille taille de grille
+     * @return Tableaux des trois meilleurs scores
+     */
     public int[] getScores(int taille){
         return scores[taille-4];
     }
@@ -107,10 +115,18 @@ public class Modele{
         insertNew();
     }
 
+    /**
+     * Retourne la taille de la grille actuelle
+     * @return taille de la grille
+     */
     public int getTaille(){
         return grid.length;
     }
 
+    /**
+     * Retourne la grille de jeu actuelle
+     * @return Tableaux à deux dimensions de Int
+     */
     public int[][] getGrille(){
         return grid;
     }
@@ -120,6 +136,7 @@ public class Modele{
      */
     public void insertNew(){
         int nb =0;
+        //On compte les cases vides sur la grille
         for (int y=0; y<grid.length; y++) {
             for (int x=0; x<grid[y].length; x++) {
                 if (grid[y][x]==0) {
@@ -127,26 +144,24 @@ public class Modele{
                 }
             }
         }
-        boolean insert = false;
-        while(insert==false){
-            for (int y=0; y<grid.length; y++){
-                for (int x=0; x<grid[y].length; x++){
-                    if (grid[y][x]==0 && insert==false){
-                        if ((int)(Math.random()*nb) == 1  || nb == 1){
-                            grid[y][x]=2;
-                            insert=true;
+        //On obtient donc une chance de 1/nombre de cases vides d'avoir un 2 dans une case.
+
+        while(true) { // On recommence tant que l'on a pas insérer de 2
+            for (int y = 0; y < grid.length; y++) {
+                for (int x = 0; x < grid[y].length; x++) {
+                    if (grid[y][x] == 0) {
+                        if ((int) (Math.random() * nb) == 1 || nb == 1) {
+                            grid[y][x] = 2;
+                            return;
                         }
                     }
-                    if (insert)
-                        break;
                 }
-                if (insert)
-                    break;
             }
         }
     }
 
     private void resetGridB(){
+        //Met toute la grille de boolean à false
         for (int y=0; y<gridb.length; y++) {
             for (int x=0; x<gridb.length; x++) {
                 gridb[y][x] = false;
@@ -165,9 +180,13 @@ public class Modele{
         resetGridB();
         do {
             change = false;
+            //Selon le sens du déplacement les cases doivent fusionner dans des sens différents
+            //Par exemple si on va à droite on a besoin de fusionner de droite à gauche
             for (int y=((dirY>0) ? grid.length-1 : 0); (dirY>0 && y>=0) || (dirY<=0 && y< grid.length); y=y+((dirY>0)? -1:1) )  {
                 for (int x=((dirX>0) ? grid.length-1 : 0); (dirX>0 && x>=0) || (dirX<=0 && x< grid.length); x=x+((dirX>0)? -1:1) ) {
+                    //Si la case n'est pas vide ou si le déplacement ne sort pas de la grille on tente de la déplacer
                     if (grid[y][x] != 0 && (y+dirY) >= 0 && (y+dirY) < grid.length && (x+dirX) >= 0 && (x+dirX) < grid.length){
+                        //Si la case n'a pas encore été fusionnée et si la case visée et la meme on les fusionne
                         if ((!gridb[y][x] && grid[y][x] == grid[y+dirY][x+dirX])) {
                             //On doit fusionner
                             grid[y+dirY][x+dirX]+=grid[y][x];
@@ -177,6 +196,7 @@ public class Modele{
                             change = true;
                             bouge=true;
                         }
+
                         if (grid[y+dirY][x+dirX]==0){
                             grid[y+dirY][x+dirX]+=grid[y][x];
                             gridb[y+dirY][x+dirX]=gridb[y][x];
